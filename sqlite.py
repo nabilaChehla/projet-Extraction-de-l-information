a52828702849300656dea4fb475cb32e72081ab6
nabilaChehla <136928671+nabilaChehla@users.noreply.github.com>
2023-12-25 22:06:13 +0100

    sqlite.py : fixed + orderd

--- sqlite.py
+++ sqlite.py
@@ -3,25 +3,33 @@ import codecs
 import re
 import os
 
-
 if os.path.exists("extraction.db"):
     os.remove("extraction.db")
 
-
 file = codecs.open(r"corpus-medical_snt\concord.html", "r", "utf-8")
+content = file.read()
+file.close()
 
-l = file.read()
+# Use a set to store unique values
+unique_posologies = set()
 
-fin = re.findall("<a[^>]*>(.*?)<\/a>", l)
+# Use a compiled regex pattern for better performance
+pattern = re.compile(r"<a[^>]*>(.*?)<\/a>")
+
+matches = pattern.findall(content)
 
 con = sqlite3.connect("extraction.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE EXTRACTION ( ID INTEGER PRIMARY KEY, Posologie TEXT )")
-i = 1
-for x in fin:
-    print(f"ID : {i} POSOLOGIE : {x[2]}\n")
-    cur.execute(f"INSERT INTO EXTRACTION VALUES ({str(i)},'{x[2]}')")
-    i = i + 1
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS EXTRACTION (ID INTEGER PRIMARY KEY, Posologie TEXT)"
+)
+
+# Sort the unique values alphabetically
+sorted_posologies = sorted(set(matches), key=lambda x: x.lower())
+
+for i, posologie in enumerate(sorted_posologies, start=1):
+    print(f"ID: {i} POSOLOGIE: {posologie}\n")
+    cur.execute("INSERT INTO EXTRACTION (ID, Posologie) VALUES (?, ?)", (i, posologie))
 
 con.commit()
 cur.close()
